1edffc37762fbf0bdd2f27493d8c57745f70f335
Melchs40 <samscodingjourney@gmail.com>
2022-12-10 10:58:06 -0600

    Edit index.html and test.js

---
+++ test.js
@@ -0,0 +1,7 @@
+for (let i=0; i<256; i++) {
+    let square = document.createElement("div")
+    square.className = "square"
+
+    document.getElementById("container").appendChild(square)
+    console.log("hello")
+}
