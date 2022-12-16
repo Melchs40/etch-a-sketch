aff50193746919f170c7ee5769bf9c2f17d0fefb
Melchs40 <samscodingjourney@gmail.com>
2022-12-15 23:22:45 -0600

    Edit index.html and test.js

--- test.js
+++ test.js
@@ -1,7 +1,18 @@
-for (let i=0; i<256; i++) {
+let userNumber = " ";
+
+function createGrid (userNumber) {
+    for (let i=0; i<(userNumber*userNumber); i++) {
     let square = document.createElement("div")
     square.className = "square"
 
     document.getElementById("container").appendChild(square)
     console.log("hello")
 }
+}
+
+function askUser () {
+    let response = prompt("please enter a number");
+    createGrid(response);
+}
+
+button.addEventListener("click", askUser);
