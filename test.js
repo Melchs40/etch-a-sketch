ce71bf31c3c6f35a9e0fac47c8149af3e97385f9
Melchs40 <samscodingjourney@gmail.com>
2022-12-22 14:15:20 -0600

    Edit style.css and test.js

--- test.js
+++ test.js
@@ -4,10 +4,14 @@ function createGrid (userNumber) {
     for (let i=0; i<(userNumber*userNumber); i++) {
     let square = document.createElement("div");
     square.className = "square";
-    
-
+    document.getElementById("container").style.display = "flex";
+    document.getElementById("container").style.flexWrap = "wrap";
+    document.getElementById("container").style.maxHeight = `${userNumber*30}px`;
+    document.getElementById("container").style.maxWidth = `${userNumber*30}px`;
+    document.getElementById("container").style.minHeight = `${userNumber*30}px`;
+    document.getElementById("container").style.minWidth = `${userNumber*30}px`;
     document.getElementById("container").appendChild(square);
-}
+    }
 }
 
 function askUser () {
@@ -16,11 +20,3 @@ function askUser () {
 }
 
 button.addEventListener("click", askUser);
-
-const squares = document.querySelectorAll("div.square");
-
-squares.forEach((square) => {
-    square.addEventListener("mouseover", () => {
-        square.style.backgroundColor = "green";
-    });
-});
